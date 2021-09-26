64f69cd62f87f584d8f781266937fc575ab5d9b6
DCarr40 <davidwcarruthers@yahoo.com>
2021-09-26 03:51:21 -0400

    working on a style sheet js component

--- App.js
+++ App.js
@@ -1,30 +1,26 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, Image } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 import MyButton from "./components/Buttons/MyButton";
+import { HomePizzaImg } from "./components/Buttons/HomePizzaImg";
+import MyStyleSheet from "./MyStyleSheet";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          url: "https://picsum.photos/200/300",
-        }}
-      />
-      <Text>MobilePizzaApp</Text>
+    <SafeAreaView>
+      <HomePizzaImg />
+      <Text style={styles.title}>MobilePizzaApp</Text>
       <MyButton />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
