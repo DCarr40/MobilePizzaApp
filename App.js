656e6b4c4a38f884128b285fadd32109ecf7c911
DCarr40 <davidwcarruthers@yahoo.com>
2021-09-26 21:36:34 -0400

    modified how styles are passed

--- App.js
+++ App.js
@@ -3,24 +3,36 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 import MyButton from "./components/Buttons/MyButton";
 import { HomePizzaImg } from "./components/Buttons/HomePizzaImg";
-import MyStyleSheet from "./MyStyleSheet";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <HomePizzaImg />
+    <SafeAreaView style={styles.container}>
       <Text style={styles.title}>MobilePizzaApp</Text>
+      <HomePizzaImg />
       <MyButton />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#F36A06",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
---
+++ components/Buttons/HomePizzaImg.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Alert, Image, TouchableOpacity } from "react-native";
+
+export const HomePizzaImg = () => {
+  const handleImagePress = () => {
+    Alert.alert("Mobile Pizza App Image has been pressed", "", [
+      {
+        text: "Pizza",
+        onPress: () => console.log("You want the Pizza"),
+      },
+    ]);
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        handleImagePress();
+      }}
+    >
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          url: "https://media.istockphoto.com/photos/pep-saus-pizz-picture-id1324333195?b=1&k=20&m=1324333195&s=170667a&w=0&h=5_8XAcLnOIiBKnSzYuwsPgf5AmqDE8V-C2Tfzx3EJfA=",
+        }}
+      />
+    </TouchableOpacity>
+  );
+};
